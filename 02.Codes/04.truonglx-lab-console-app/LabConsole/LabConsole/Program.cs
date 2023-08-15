53d82af28366872c6b3dc95127f54ed306c609ad
truonglx <truonglx8993@gmail.com>
2023-08-15 18:18:06 +0700

    update

---
+++ 02.Codes/04.truonglx-lab-console-app/LabConsole/LabConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter first number");
+var firstNumberStr = Console.ReadLine();
+Console.WriteLine("Enter seconds number");
+var secondNumberStr = Console.ReadLine();
+int.TryParse(firstNumberStr, out var firstNb);
+int.TryParse(secondNumberStr, out var second);
+var sum = firstNb + second;
+Console.WriteLine(sum);
